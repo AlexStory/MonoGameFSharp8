3f0f4cd04df3e73b42c0b90fb34e3ae917a5b8b2
Alex Story <astory11@yahoo.com>
2024-04-25 09:54:42 -0500

    initial commit

---
+++ Game1.fs
@@ -0,0 +1,32 @@
+﻿namespace MyGame
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+
+type Game1 () as this =
+    inherit Game ()
+
+    let _graphics = new GraphicsDeviceManager(this)
+    let mutable _spriteBatch = Unchecked.defaultof<SpriteBatch>
+
+    do
+        this.Content.RootDirectory <- "Content"
+        this.IsMouseVisible <- true
+
+    override _.Initialize () =
+        base.Initialize()
+
+    override _.LoadContent () =
+        _spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+
+    override _.Update (gameTime: GameTime) =
+        if GamePad.GetState(PlayerIndex.One).Buttons.Back = ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape) then
+            this.Exit()
+
+        base.Update(gameTime)
+
+    override this.Draw (gameTime: GameTime) =
+        this.GraphicsDevice.Clear(Color.CornflowerBlue)
+
+        base.Draw(gameTime)
---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿open MyGame
+open System
+
+[<STAThread; EntryPoint>]
+let main argv =
+        use game = new Game1()
+        game.Run ()
+        0
